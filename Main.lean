45e80f0b670221a9fc9eb2608a5d3120f64a1132
David Thrane Christiansen <david@davidchristiansen.dk>
2024-10-19 16:14:41 +0200

    feat: add Lean logo

--- Main.lean
+++ Main.lean
@@ -11,7 +11,8 @@ where
   config := {
     extraFiles := [("static", "static")],
     extraCss := ["/static/colors.css", "/static/theme.css", "/static/print.css", "/static/fonts/source-serif/source-serif-text.css", "/static/fonts/source-code-pro/source-code-pro.css", "/static/katex/katex.min.css"],
-    extraJs := ["/static/katex/katex.min.js", "/static/math.js", "/static/print.js"]
-    emitTeX := false
-    emitHtmlSingle := true -- for proofreading
+    extraJs := ["/static/katex/katex.min.js", "/static/math.js", "/static/print.js"],
+    emitTeX := false,
+    emitHtmlSingle := true, -- for proofreading
+    logo := some "/static/lean_logo.svg"
   }
