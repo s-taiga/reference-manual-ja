b7c739e5c185a0f3f542593812b03c7eaa5d8dec
David Thrane Christiansen <david@davidchristiansen.dk>
2024-10-19 14:59:24 +0200

    chore: adopt branding of current Lean web page

--- Main.lean
+++ Main.lean
@@ -10,7 +10,7 @@ def main :=
 where
   config := {
     extraFiles := [("static", "static")],
-    extraCss := ["/static/colors.css", "/static/theme.css", "/static/print.css", "/static/inter/inter.css", "/static/firacode/fira_code.css", "/static/katex/katex.min.css"],
+    extraCss := ["/static/colors.css", "/static/theme.css", "/static/print.css", "/static/fonts/source-serif/source-serif-text.css", "/static/fonts/source-code-pro/source-code-pro.css", "/static/katex/katex.min.css"],
     extraJs := ["/static/katex/katex.min.js", "/static/math.js", "/static/print.js"]
     emitTeX := false
     emitHtmlSingle := true -- for proofreading
