7c244e0642883d2173c76640c5659930f5c42c62
s-taiga <56785231+s-taiga@users.noreply.github.com>
2024-12-26 23:44:28 +0900

    Monad_API翻訳 (#36)
    * 翻訳開始
    
    * 翻訳完了

--- Manual/Monads/API.lean
+++ Manual/Monads/API.lean
@@ -19,16 +19,35 @@ set_option pp.rawOnError true
 
 set_option linter.unusedVariables false
 
+/-
 #doc (Manual) "API Reference" =>
+-/
+#doc (Manual) "API リファレンス（API Reference）" =>
 
+:::comment
 In addition to the general functions described here, there are some functions that are conventionally defined as part of the API of in the namespace of each collection type:
  * `mapM` maps a monadic function.
  * `forM` maps a monadic function, throwing away the result.
  * `filterM` filters using a monadic predicate, returning the values that satisfy it.
 
 
-::::example "Monadic Collection Operations"
+:::
+
+ここで説明する一般的な関数に加えて、各コレクション型の名前空間の API の一部として慣習的に定義されている関数がいくつかあります：
+ * `mapM` はモナド関数をマップします。
+ * `forM` はモナド関数をマップし、結果を捨てます。
+ * `filterM` はモナド述語を使ってフィルタリングを行い、それを満たす値を返します。
+
+:::comment
+::example "Monadic Collection Operations"
+:::
+::::example "モナドのコレクションに対する操作"
+:::comment
 {name}`Array.filterM` can be used to write a filter that depends on a side effect.
+
+:::
+
+{name}`Array.filterM` を使用すると、副作用に依存したフィルタを書くことができます。
 
 :::ioExample
 ```ioLean
@@ -68,19 +87,39 @@ These values were kept:
 :::
 ::::
 
+:::comment
 # Discarding Results
+:::
 
+# 結果の破棄（Discarding Results）
+
+
+:::comment
 The {name}`discard` function is especially useful when using an action that returns a value only for its side effects.
+
+:::
+
+{name}`discard` 関数は副作用のためだけに値を返すアクションを使うときに便利です。
 
 {docstring discard}
 
+:::comment
 # Control Flow
+:::
+
+# フローの制御（Control Flow）
+
 
 {docstring guard}
 
 {docstring optional}
 
+:::comment
 # Lifting Boolean Operations
+:::
+
+# 真偽値演算子の持ち上げ（Lifting Boolean Operations）
+
 
 {docstring andM}
 
@@ -88,18 +127,38 @@ The {name}`discard` function is especially useful when using an action that retu
 
 {docstring notM}
 
+:::comment
 # Kleisli Composition
+:::
 
+# Kleisli 合成（Kleisli Composition）
+
+
+:::comment
 {deftech}_Kleisli composition_ is the composition of monadic functions, analogous to {name}`Function.comp` for ordinary functions.
+
+:::
+
+{deftech}_Kleisli 合成_ （Kleisli composition）はモナド関数の合成で、普通の関数の {name}`Function.comp` に似ています。
 
 {docstring Bind.kleisliRight}
 
 {docstring Bind.kleisliLeft}
 
+:::comment
 # Re-Ordered Operations
+:::
 
+# 並べ替え操作（Re-Ordered Operations）
+
+
+:::comment
 Sometimes, it can be convenient to partially apply a function to its second argument.
 These functions reverse the order of arguments, making it this easier.
+
+:::
+
+関数の第2引数に関数を部分適用すると便利な場合があります。これらの関数は引数の順序を逆にすることでこれを簡単に行うことができます。
 
 {docstring Functor.mapRev}
 
